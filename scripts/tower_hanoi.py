65d070cb786a9ad28322fd38866ad898890b2e25
Wolfgang Wiedmeyer <wolfgang.wiedmeyer@kit.edu>
2018-03-19 21:15:37 +0100

    recursive solution
    Signed-off-by: Wolfgang Wiedmeyer <wolfgang.wiedmeyer@kit.edu>

--- scripts/tower_hanoi.py
+++ scripts/tower_hanoi.py
@@ -18,129 +18,94 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# 1->3, 1->2, 3->2, 1->3, 2->1, 2->3, 1->3
-
 import rospy
 from moveit_testing_sender.srv import *
 from geometry_msgs.msg import Pose
 
-def play_tower_of_hanoi():
+
+def hanoi(n, source, helper, target):
+    print source, helper, target
+    if n > 0:
+        # move tower of size n - 1 to helper:
+        hanoi(n - 1, source, target, helper)
+
+        # move disk from source peg to target peg
+        if source["disks"]:
+            move_disk(source, target)
+
+        # move tower of size n-1 from helper to target
+        hanoi(n - 1, helper, source, target)
+
+def move_disk(start, dest):
     pose_above = Pose()
     pose_grip = Pose()
 
-    # above table: z: 0.12
-    # 1->3
-    pose_above.position.z = 0.2
+    # pick disk up
+    rospy.loginfo("start has %d disks", start["disks"])
+    pose_above.position.z = 0.08
     pose_above.position.x = 0.5
-    pose_above.position.y = 0.0
+    pose_above.position.y = stack_pos[start["pos"]]
     pose_above.orientation.y = 1.0
-    pose_grip.position.z = 0.166
+    pose_grip.position.z = heights[start["disks"]]
     pose_grip.position.x = 0.5
-    pose_grip.position.y = 0.0
+    pose_grip.position.y = stack_pos[start["pos"]]
     pose_grip.orientation.y = 1.0
 
     pick_place = rospy.ServiceProxy('pick_place', PickPlace)
     resp = pick_place(pose_above, pose_grip, True)
-    rospy.loginfo("pick place job finished")
+    rospy.loginfo("pick up job finished")
+    start["disks"] -= 1
 
-    pose_above.position.y = 0.2
-    pose_grip.position.z = 0.122
-    pose_grip.position.y = 0.2
+    # place disk
+    rospy.loginfo("dest has %d disks", dest["disks"])
+    pose_above.position.y = stack_pos[dest["pos"]]
+    pose_grip.position.z = heights[dest["disks"]+1]
+    pose_grip.position.y = stack_pos[dest["pos"]]
     resp = pick_place(pose_above, pose_grip, False)
-    rospy.loginfo("pick place job finished")
+    rospy.loginfo("place job finished")
+    dest["disks"] += 1
 
-    # 1->2
-    pose_above.position.y = 0.0
-    pose_grip.position.z = 0.144
-    pose_grip.position.y = 0.0
+def cleanup():
+    pose_above = Pose()
+    pose_grip = Pose()
+
+    # pick tower up
+    pose_above.position.z = 0.03
+    pose_above.position.x = 0.5
+    pose_above.position.y = stack_pos[target["pos"]]
+    pose_above.orientation.y = 1.0
+    pose_grip.position.z = heights[1]
+    pose_grip.position.x = 0.5
+    pose_grip.position.y = stack_pos[target["pos"]]
+    pose_grip.orientation.y = 1.0
+
+    pick_place = rospy.ServiceProxy('pick_place', PickPlace)
     resp = pick_place(pose_above, pose_grip, True)
-    rospy.loginfo("pick place job finished")
+    rospy.loginfo("pick up job finished")
 
-    pose_above.position.y = 0.1
-    pose_grip.position.z = 0.122
-    pose_grip.position.y = 0.1
+    # place disk
+    pose_above.position.y = stack_pos[source["pos"]]
+    pose_grip.position.y = stack_pos[source["pos"]]
+    pick_place = rospy.ServiceProxy('pick_place', PickPlace)
     resp = pick_place(pose_above, pose_grip, False)
-    rospy.loginfo("pick place job finished")
+    rospy.loginfo("place job finished")
 
-    # 3->2
-    pose_above.position.y = 0.2
-    pose_grip.position.z = 0.122
-    pose_grip.position.y = 0.2
-    resp = pick_place(pose_above, pose_grip, True)
-    rospy.loginfo("pick place job finished")
-
-    pose_above.position.y = 0.1
-    pose_grip.position.z = 0.144
-    pose_grip.position.y = 0.1
+    # move a little higher before homing
+    pose_above.position.z = 0.1
+    pose_grip.position.z = 0.1
     resp = pick_place(pose_above, pose_grip, False)
-    rospy.loginfo("pick place job finished")
+    rospy.loginfo("ready to home")
 
-    # 1->3
-    pose_above.position.y = 0.0
-    pose_grip.position.z = 0.122
-    pose_grip.position.y = 0.0
-    resp = pick_place(pose_above, pose_grip, True)
-    rospy.loginfo("pick place job finished")
 
-    pose_above.position.y = 0.2
-    pose_grip.position.z = 0.122
-    pose_grip.position.y = 0.2
-    resp = pick_place(pose_above, pose_grip, False)
-    rospy.loginfo("pick place job finished")
+source = {"pos": 1, "disks": 3}
+target = {"pos": 3, "disks": 0}
+helper = {"pos": 2, "disks": 0}
 
-    # 2->1
-    pose_above.position.y = 0.1
-    pose_grip.position.z = 0.144
-    pose_grip.position.y = 0.1
-    resp = pick_place(pose_above, pose_grip, True)
-    rospy.loginfo("pick place job finished")
-
-    pose_above.position.y = 0.0
-    pose_grip.position.z = 0.122
-    pose_grip.position.y = 0.0
-    resp = pick_place(pose_above, pose_grip, False)
-    rospy.loginfo("pick place job finished")
-
-    # 2->3
-    pose_above.position.y = 0.1
-    pose_grip.position.z = 0.122
-    pose_grip.position.y = 0.1
-    resp = pick_place(pose_above, pose_grip, True)
-    rospy.loginfo("pick place job finished")
-
-    pose_above.position.y = 0.2
-    pose_grip.position.z = 0.144
-    pose_grip.position.y = 0.2
-    resp = pick_place(pose_above, pose_grip, False)
-    rospy.loginfo("pick place job finished")
-
-    # 1->3
-    pose_above.position.y = 0.0
-    pose_grip.position.z = 0.122
-    pose_grip.position.y = 0.0
-    resp = pick_place(pose_above, pose_grip, True)
-    rospy.loginfo("pick place job finished")
-
-    pose_above.position.y = 0.2
-    pose_grip.position.z = 0.166
-    pose_grip.position.y = 0.2
-    resp = pick_place(pose_above, pose_grip, False)
-    rospy.loginfo("pick place job finished")
-
-    # reset
-    pose_above.position.y = 0.2
-    pose_grip.position.z = 0.122
-    pose_grip.position.y = 0.2
-    resp = pick_place(pose_above, pose_grip, True)
-    rospy.loginfo("pick place job finished")
-
-    pose_above.position.y = 0.0
-    # pose_above.position.z = 0.15
-    pose_grip.position.z = 0.122
-    pose_grip.position.y = 0.0
-    resp = pick_place(pose_above, pose_grip, False)
-    rospy.loginfo("pick place job finished")
+heights = {3: 0.048, 2: 0.026, 1: 0.004, 0: 0.004}
+stack_pos = {1: -0.2, 2: 0.0, 3: 0.2}
 
 if __name__ == '__main__':
     rospy.init_node('tower_hanoi')
-    play_tower_of_hanoi()
+
+    hanoi(source["disks"], source, helper, target)
+    cleanup()
