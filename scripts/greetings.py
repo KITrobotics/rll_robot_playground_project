2d64220245dce6c8a340ed0570813cce294f4a8a
Wolfgang Wiedmeyer <wolfgang.wiedmeyer@kit.edu>
2018-06-13 20:42:43 +0200

    add a little rotation in joint 5
    Signed-off-by: Wolfgang Wiedmeyer <wolfgang.wiedmeyer@kit.edu>

--- scripts/greetings.py
+++ scripts/greetings.py
@@ -26,7 +26,7 @@ def greetings():
     joint_2 = 1.832
     joint_3 = -0.3
     joint_4 = 2.01
-    joint_5 = -1.396
+    joint_5 = -2
     joint_6 = 0.0
     joint_7 = 1.4835
 
@@ -37,6 +37,7 @@ def greetings():
         return False
 
     joint_3 = 0.3
+    joint_5 = -0.7
     resp = move_joints(joint_1, joint_2, joint_3, joint_4, joint_5, joint_6, joint_7)
     if resp.success == False:
         rospy.logerr("moving to second joint pos failed")
