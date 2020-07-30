b40b2b2f4edfca21e52f60148e6c21fe7c86a3d3
Wolfgang Wiedmeyer <wolfgang.wiedmeyer@kit.edu>
2020-07-30 13:02:20 +0200

    CI: update paths for linting
    Signed-off-by: Wolfgang Wiedmeyer <wolfgang.wiedmeyer@kit.edu>

--- .ci/linting.sh
+++ .ci/linting.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-CLANG_TIDY_CMD="run-clang-tidy-7.py -checks=-readability-function-size -p /root/catkin_ws/build"
+CLANG_TIDY_CMD="run-clang-tidy-7.py -checks=-readability-function-size -p /root/target_ws/build"
 CLANG_TIDY_CHECK_PACKAGES="rll_robot_playground_project"
 
 for package in $CLANG_TIDY_CHECK_PACKAGES
@@ -11,5 +11,5 @@ do
 done
 
 pip -q install 'pylint<2.0.0'
-. /root/catkin_ws/install/setup.bash
+. /root/target_ws/install/setup.bash
 find * -iname '*.py' | xargs python -m pylint --disable=duplicate-code,too-many-statements
